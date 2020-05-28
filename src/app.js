e1e5fdea73ab12c14ba35467a0d6a9bd3bfb9e3e
fatukunda <fatukunda@gmail.com>
2020-05-28 11:14:16 +0300

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import config from "dotenv";
+import cors from "cors";
 import userRouter from "./routes/userRouter";
 import bookRouter from "./routes/bookRouter";
 
@@ -8,6 +9,7 @@ import("./config/database");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/books", bookRouter);
 
