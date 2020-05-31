2c986c1b59224e33cd5ba6f12f58be4e1433927b
fatukunda <fatukunda@gmail.com>
2020-05-31 10:16:51 +0300

    Modify Limit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(paginate.middleware(5, 10))
+app.use(paginate.middleware(4, 10))
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/books", bookRouter);
 
